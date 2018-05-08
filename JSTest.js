10e37b2806cf60057d91b7896c4bc9547b1db76d
bakht <haker-1996@mail.ru>
2018-05-09 00:21:59 +0700

    Try web

--- JSTest.js
+++ JSTest.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+jQuery('document').ready(function () {
+    jQuery('button').on('click', function () {
+        var firstNum, secondNum;
+        firstNum = parseInt( jQuery('#firstInput').val());
+        secondNum =parseInt( jQuery('#secondInput').val());
+        alert(firstNum + secondNum);
+
+    });
+})
